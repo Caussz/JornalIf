2ba53c9e296f52eea10201f08d92ca0c654c773f
Cauã Vinicius <77009854+Caussz@users.noreply.github.com>
2022-08-19 21:43:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -25,13 +25,13 @@ router.get('/css/nav.css', (req, res) => {
 router.get('/js/index.js', (req, res) => {
     res.sendFile(__path + '/js/index.js')
 })
-router.get('/image/stevejobsiphone2g.jpg', (req, res) => {
+router.get('/images/stevejobsiphone2g.jpg', (req, res) => {
     res.sendFile(__path + '/image/stevejobsiphone2g.jpg')
 })
-router.get('./image/caua-juliette.jpg', (req, res) => {
+router.get('/images/caua-juliette.jpg', (req, res) => {
     res.sendFile(__path + '/image/caua-juliette.jpg')
 })
-router.get('./image/luiz-juliette.jpg', (req, res) => {
+router.get('/images/luiz-juliette.jpg', (req, res) => {
     res.sendFile(__path + '/image/luiz-juliette.jpg')
 })
 module.exports = router
